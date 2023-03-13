476be0af62b67b9533134250354a42cdb2708e99
paphuc <29089718+paphuc@users.noreply.github.com>
2023-03-13 19:29:31 +0700

    update config

--- WRITEUP.md
+++ WRITEUP.md
@@ -1,12 +1,20 @@
 # Write-up Template
 
 ### Analyze, choose, and justify the appropriate resource option for deploying the app.
+##### Cost
+    VM: Higher costs
+    App service: pay as you go price
+##### Scalability
+    VM: Ability to access to server configuration
+    App service: Can not access to server configuration, does not support multiple deployment
+##### Availability
+    VM: need to cluster multiple virtual machines to provide high availability
+    App service: already supported high availability
+##### Workflow
+    VM: requires more effort with a extra steps
+    App service: easy integrated in ci/cd of git 
 
-*For **both** a VM or App Service solution for the CMS app:*
-- *Analyze costs, scalability, availability, and workflow*
-- *Choose the appropriate solution (VM or App Service) for deploying the app*
-- *Justify your choice*
-
+=> Using App service because the application is quite simple, no need to install the specific apps or acess to server
 ### Assess app changes that would change your decision.
 
-*Detail how the app and any other needs would have to change for you to change your decision in the last section.* 
+In case I have to control the operating system or application environment, or where the application requires a dedicated server for security or other reasons, then I will change my mind to use a VM.
--- config.py
+++ config.py
@@ -5,21 +5,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'articlecms123'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'u4DgYsVXs8M7EnUnTou6SQhRKPAZtpwQ94u2c5IQMr9jP/FyoG30fyTaZU2f/vfXktCK9LGdujIV+AStmp7mZQ=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'articlecms.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'article-cms'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'articleadmin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Abc123456'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
+    CLIENT_SECRET = "vo38Q~SUsi_hTvXQPPruqkk5tlf731efIUdKDaj2"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -30,7 +30,7 @@ class Config(object):
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = "318d38cf-c04e-439a-9b72-ce053ecf9f1b"
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
